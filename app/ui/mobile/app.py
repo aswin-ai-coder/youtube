8b1936dd4df90615a8ddd618fb6bfa350327f50d
Aswin Prakash <228343533+aswin-ai-coder@users.noreply.github.com>
2026-08-29 20:37:28 +0530

    feat: expose favorites in mobile app

--- app/ui/mobile/app.py
+++ app/ui/mobile/app.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 
 from app.ui.mobile.download import DownloadPage
+from app.ui.mobile.favorites import FavoritesPage
 from app.ui.mobile.history import HistoryPage
 from app.ui.mobile.queue import QueuePage
 from app.ui.mobile.settings import SettingsPage
@@ -17,6 +18,7 @@ class YouTubeDownloaderMobileApp(App):
         tabs.add_widget(DownloadPage(text="Downloads"))
         tabs.add_widget(QueuePage(text="Queue"))
         tabs.add_widget(HistoryPage(text="History"))
+        tabs.add_widget(FavoritesPage(text="Favorites"))
         tabs.add_widget(SettingsPage(text="Settings"))
         return tabs
 
